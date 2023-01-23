2ab0dac6f13dbb1a97844be7b5c7dabf640d45c1
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-01-23 18:29:19 -0500

    Created a basic library.

---
+++ include/mylibrary.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace mylibrary
+{
+    void say_hello();
+    
+    int add(int a, int b);
+}
---
+++ src/mylibrary.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+namespace mylibrary
+{
+    void say_hello()
+    {
+        std::cout << "Hello!\n";
+    }
+    
+    int add(int a, int b)
+    {
+        return a + b;
+    }
+}
